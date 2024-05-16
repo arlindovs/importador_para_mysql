923f2de550ead8ce21483b509a0f8931e7373850
Arlindo Silva <arlindovicente@gmail.com>
2024-05-15 21:39:05 -0300

    implementação do sql server

--- app/gui.py
+++ app/gui.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from tkinter import ttk, messagebox, filedialog
 from datetime import datetime
 
-from utils import import_data_from_firebird_to_mysql
+from firebird import import_data_from_firebird_to_mysql
+from sqlserver import import_structure_and_data_from_sqlserver_to_mysql
 
 class DataImporterGUI:
     def __init__(self, root):
@@ -42,10 +43,39 @@ class DataImporterGUI:
         # Separator
         separator = ttk.Separator(self.root, orient='horizontal')
         separator.grid(row=1, column=0, columnspan=3, sticky="ew", pady=10)
+        
+        # Frame para os dados de conexão do SqlServer
+        sqlserver_frame = tk.LabelFrame(self.root, text="Origem: Dados de Conexão do SqlServer", padx=10, pady=10)
+        sqlserver_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        sqlserver_frame.config(bg="#f0f0f0")  # Cor de fundo
+        
+        # SqlServer DB File
+        file_label = tk.Label(sqlserver_frame, text="DB File:", bg="#f0f0f0")  # Cor de fundo
+        file_label.grid(row=0, column=0, sticky="w")
+        self.sqlserver_file_entry = tk.Entry(sqlserver_frame, width=40)
+        self.sqlserver_file_entry.grid(row=0, column=1, padx=5, pady=5, sticky="we")
+        sqlserver_file_button = tk.Button(sqlserver_frame, text="Browse", command=self.browse_sqlserver_file)
+        sqlserver_file_button.grid(row=0, column=2, padx=5, pady=5)
+
+        # SqlServer User
+        user_label = tk.Label(sqlserver_frame, text="Usuário:", bg="#f0f0f0")  # Cor de fundo
+        user_label.grid(row=1, column=0, sticky="w")
+        self.sqlserver_user_entry = tk.Entry(sqlserver_frame)
+        self.sqlserver_user_entry.grid(row=1, column=1, padx=5, pady=5, sticky="we")
+
+        # SqlServer Password
+        password_label = tk.Label(sqlserver_frame, text="Senha:", bg="#f0f0f0")  # Cor de fundo
+        password_label.grid(row=2, column=0, sticky="w")
+        self.sqlserver_password_entry = tk.Entry(sqlserver_frame, show="*")
+        self.sqlserver_password_entry.grid(row=2, column=1, padx=5, pady=5, sticky="we")
+
+        # Separator
+        separator = ttk.Separator(self.root, orient='horizontal')
+        separator.grid(row=3, column=0, columnspan=3, sticky="ew", pady=10)
 
         # Frame para os dados de conexão do MySQL
         mysql_frame = tk.LabelFrame(self.root, text="Destino: Dados de Conexão do MySQL", padx=10, pady=10)
-        mysql_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        mysql_frame.grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
         mysql_frame.config(bg="#f0f0f0")  # Cor de fundo
 
         # MySQL Host
@@ -84,30 +114,45 @@ class DataImporterGUI:
 
         # Label para mensagens de erro
         self.error_label = tk.Label(self.root, text="", fg="red", bg="#f0f0f0")  # Cor de fundo
-        self.error_label.grid(row=4, column=0, padx=10, pady=(0, 10), sticky="ew")
+        self.error_label.grid(row=5, column=0, padx=10, pady=(0, 10), sticky="ew")
 
         # Connect Button
         self.connect_button = tk.Button(mysql_frame, text="Conectar", command=self.connect_to_mysql, bg="#007bff", fg="white")  # Cores de fundo e texto
         self.connect_button.grid(row=4, column=2, columnspan=2, pady=(5, 5))
+        
+        # Adicionando botões de opção para escolher a origem da importação
+        self.source_selection_label = tk.Label(self.root, text="Escolha a Origem:", bg="#f0f0f0")  # Cor de fundo
+        self.source_selection_label.grid(row=6, column=0, padx=10, pady=10, sticky="ew")
+
+        self.source_selection_var = tk.StringVar()
+        self.firebird_radio = tk.Radiobutton(self.root, text="Firebird", variable=self.source_selection_var, value="Firebird")
+        self.firebird_radio.grid(row=6, column=1, padx=5, pady=10, sticky="ew")
+        self.sqlserver_radio = tk.Radiobutton(self.root, text="SQL Server", variable=self.source_selection_var, value="SQL Server")
+        self.sqlserver_radio.grid(row=6, column=2, padx=5, pady=10, sticky="ew")
 
         # Submit Button
         self.submit_button = tk.Button(self.root, text="Executar", command=self.submit, bg="#28a745", fg="white")  # Cores de fundo e texto
-        self.submit_button.grid(row=5, column=0, padx=10, pady=10, sticky="ew")
+        self.submit_button.grid(row=7, column=0, padx=10, pady=10, sticky="ew")
         
         # Progress Bar
         self.progress_bar = ttk.Progressbar(self.root, orient="horizontal", length=200, mode="determinate")
-        self.progress_bar.grid(row=6, column=0, padx=10, pady=10, sticky="ew")
+        self.progress_bar.grid(row=8, column=0, padx=10, pady=10, sticky="ew")
         self.progress_bar["value"] = 0
 
         # Footer
         current_year = datetime.now().year
         footer_label = tk.Label(self.root, text=f"©{current_year} Copyright - CodeCoffee", anchor="e", bg="#f0f0f0")  # Cor de fundo
-        footer_label.grid(row=7, column=0, padx=10, pady=(0, 10), sticky="ew")
+        footer_label.grid(row=9, column=0, padx=10, pady=(0, 10), sticky="ew")
 
     def browse_file(self):
         file_path = filedialog.askopenfilename(filetypes=(("Firebird Database Files", "*.fdb"), ("All files", "*.*")))
         self.file_entry.delete(0, tk.END)
         self.file_entry.insert(0, file_path)
+        
+    def browse_sqlserver_file(self):
+        file_path = filedialog.askopenfilename(filetypes=(("SQL Server Database Files", "*.bak"), ("All files", "*.*")))
+        self.sqlserver_file_entry.delete(0, tk.END)
+        self.sqlserver_file_entry.insert(0, file_path)
 
     def connect_to_mysql(self):
         try:
@@ -139,44 +184,80 @@ class DataImporterGUI:
         # Destacar os campos que precisam ser preenchidos
         self.highlight_required_fields()
         
-    def highlight_required_fields(self):
-        required_fields = [self.file_entry, self.firebird_user_entry, self.firebird_password_entry, self.host_entry, self.user_entry, self.password_entry, self.port_entry]
-        for field in required_fields:
-            if not field.get():
-                field.config(bg="pink")
-            else:
-                field.config(bg="white")
+    # def highlight_required_fields(self):
+    #     required_fields = [self.host_entry, self.user_entry, self.password_entry, self.port_entry]
+    #     for field in required_fields:
+    #         if not field.get():
+    #             field.config(bg="pink")
+    #         else:
+    #             field.config(bg="white")
 
     def submit(self):
         # Verificar se os campos obrigatórios foram preenchidos
-        if self.validate_fields():
-            # Coletar os dados de entrada
-            firebird_db_file = self.file_entry.get()
-            firebird_user = self.firebird_user_entry.get()
-            firebird_password = self.firebird_password_entry.get()
-            mysql_host = self.host_entry.get()
-            mysql_user = self.user_entry.get()
-            mysql_password = self.password_entry.get()
-            mysql_port = self.port_entry.get()
-            mysql_database = self.database_combobox.get()
-            
-            # Atualizar a barra de progresso
-            self.progress_bar["value"] = 20
-
-            # Executar a função de importação de dados
-            import_data_from_firebird_to_mysql(self.root, firebird_db_file, firebird_user, firebird_password, mysql_host, mysql_user, mysql_password, mysql_database, mysql_port)
-
-            # Atualizar a barra de progresso
-            self.progress_bar["value"] = 100
-            
-            # Exibir mensagem de confirmação
-            messagebox.showinfo("Importado", "Processo executado com sucesso!")
+        if self.validate_fields() and self.source_selection_var.get():
+            if self.source_selection_var.get() == "Firebird":
+                # Coletar os dados de entrada para Firebird
+                firebird_db_file = self.file_entry.get()
+                firebird_user = self.firebird_user_entry.get()
+                firebird_password = self.firebird_password_entry.get()
+                
+                # Coletar dados de entrada do MySQL
+                mysql_host = self.host_entry.get()
+                mysql_user = self.user_entry.get()
+                mysql_password = self.password_entry.get()
+                mysql_port = self.port_entry.get()
+                mysql_database = self.database_combobox.get()
+                
+                # Atualizar a barra de progresso
+                self.progress_bar["value"] = 20
+                
+                # Executar a função de importação do Firebird
+                import_data_from_firebird_to_mysql(self.root, firebird_db_file, firebird_user, firebird_password, mysql_host, mysql_user, mysql_password, mysql_database, mysql_port)
+                
+                # Atualizar a barra de progresso
+                self.progress_bar["value"] = 100
+                
+                # Exibir mensagem de confirmação
+                messagebox.showinfo("Importado", "Processo de importação do Firebird concluído com sucesso!")
+            elif self.source_selection_var.get() == "SQL Server":
+                # Coletar os dados de entrada para SQL Server
+                sqlserver_db_file = self.sqlserver_file_entry.get()
+                sqlserver_user = self.sqlserver_user_entry.get()
+                sqlserver_password = self.sqlserver_password_entry.get()
+                
+                # Coletar dados de entrada do MySQL
+                mysql_host = self.host_entry.get()
+                mysql_user = self.user_entry.get()
+                mysql_password = self.password_entry.get()
+                mysql_port = self.port_entry.get()
+                mysql_database = self.database_combobox.get()
+                
+                # Atualizar a barra de progresso
+                self.progress_bar["value"] = 20
+                
+                # Executar a função de importação do SQL Server
+                import_structure_and_data_from_sqlserver_to_mysql(self.root, sqlserver_db_file, sqlserver_user, sqlserver_password, mysql_host, mysql_user, mysql_password, mysql_database, mysql_port)
+                
+                # Atualizar a barra de progresso
+                self.progress_bar["value"] = 100
+                
+                # Exibir mensagem de confirmação
+                messagebox.showinfo("Importado", "Processo de importação do SQL Server concluído com sucesso!")
         else:
-            # Exibir mensagem de erro se os campos obrigatórios não estiverem preenchidos
-            messagebox.showerror("Erro", "Todos os campos são obrigatórios.")
+            # Exibir mensagem de erro se os campos obrigatórios não estiverem preenchidos ou se a origem não foi selecionada
+            messagebox.showerror("Erro", "Todos os campos são obrigatórios e uma origem deve ser selecionada.")
 
     def validate_fields(self):
-        required_fields = [self.file_entry, self.host_entry, self.user_entry, self.password_entry, self.port_entry]
+        # Verificar se a origem foi selecionada
+        if not self.source_selection_var.get():
+            return False
+        
+        required_fields = []
+        if self.source_selection_var.get() == "Firebird":
+            required_fields = [self.file_entry, self.firebird_user_entry, self.firebird_password_entry, self.host_entry, self.user_entry, self.password_entry, self.port_entry]
+        elif self.source_selection_var.get() == "SQL Server":
+            required_fields = [self.sqlserver_file_entry, self.sqlserver_user_entry, self.sqlserver_password_entry, self.host_entry, self.user_entry, self.password_entry, self.port_entry]
+        
         for field in required_fields:
             if not field.get():
                 return False
---
+++ app/sqlserver.py
@@ -0,0 +1,90 @@
+import pyodbc
+import mysql.connector
+
+def get_sqlserver_table_structure(sqlserver_conn, database_name):
+    sqlserver_cursor = sqlserver_conn.cursor()
+    tables_query = f"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_CATALOG='{database_name}'"
+    sqlserver_cursor.execute(tables_query)
+    tables = sqlserver_cursor.fetchall()
+    table_structure = {}
+    for table in tables:
+        table_name = table[0]
+        columns_query = f"SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
+        sqlserver_cursor.execute(columns_query)
+        columns = sqlserver_cursor.fetchall()
+        table_structure[table_name] = columns
+    sqlserver_cursor.close()
+    return table_structure
+
+def convert_sqlserver_field_type_to_mysql(field_type, max_length):
+    # Mapeamento de tipos de campo do SQL Server para o MySQL
+    type_mapping = {
+        'int': 'INT',
+        'nvarchar': 'VARCHAR',
+        'datetime': 'DATETIME',
+        'float': 'FLOAT',
+        'bit': 'BOOLEAN',
+        # Adicione mais mapeamentos conforme necessário
+    }
+    mysql_type = type_mapping.get(field_type, 'VARCHAR')
+    if mysql_type == 'VARCHAR' and max_length:
+        if max_length == -1:
+            return 'TEXT'
+        return f"{mysql_type}({max_length})"
+    return mysql_type
+
+
+def create_mysql_tables_from_sqlserver(mysql_conn, table_structure):
+    mysql_cursor = mysql_conn.cursor()
+    for table_name, columns in table_structure.items():
+        create_table_query = f"CREATE TABLE IF NOT EXISTS `{table_name}` ("
+        for column in columns:
+            column_name, data_type, max_length = column
+            column_type = convert_sqlserver_field_type_to_mysql(data_type, max_length)
+            create_table_query += f"`{column_name}` {column_type}, "
+        create_table_query = create_table_query.rstrip(', ') + ");"
+        mysql_cursor.execute(create_table_query)
+    mysql_cursor.close()
+
+
+def import_structure_and_data_from_sqlserver_to_mysql(sqlserver_database, sqlserver_host, sqlserver_user,sqlserver_password, mysql_host, mysql_user, mysql_password, mysql_database, mysql_port):
+    
+    # Conectar ao banco de dados SqlServer
+    sqlserver_conn = pyodbc.connect(
+        driver='{SQL Server}',
+        server=sqlserver_host,
+        database=sqlserver_database,
+        uid=sqlserver_user,
+        pwd=sqlserver_password
+    )
+
+    # Conectar ao banco de dados MySQL
+    mysql_conn = mysql.connector.connect(
+        host=mysql_host,
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_database,
+        port=mysql_port
+    )
+    # Obtendo estrutura das tabelas do SQL Server
+    table_structure = get_sqlserver_table_structure(sqlserver_conn, sqlserver_database)
+    
+    # Criando tabelas no MySQL
+    create_mysql_tables_from_sqlserver(mysql_conn, table_structure)
+    
+    # Importando dados
+    sqlserver_cursor = sqlserver_conn.cursor()
+    mysql_cursor = mysql_conn.cursor()
+    
+    for table_name in table_structure.keys():
+        sqlserver_cursor.execute(f"SELECT * FROM {table_name}")
+        rows = sqlserver_cursor.fetchall()
+        
+        for row in rows:
+            placeholders = ', '.join(['%s'] * len(row))
+            mysql_cursor.execute(f"INSERT INTO `{table_name}` VALUES ({placeholders})", row)
+    
+    mysql_conn.commit()
+    
+    sqlserver_cursor.close()
+    mysql_cursor.close()
